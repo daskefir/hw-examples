af6029389e7f7e446c6ce724881f5099abc7522b
Denis <fiwhy@mail.ru>
2017-12-19 17:33:02 +0200

    Added view

--- army.js
+++ army.js
@@ -1,6 +1,5 @@
 var army = [];
 
-
 function createResource(type, health, distance) {
     return {
         type: type,
@@ -15,29 +14,35 @@ function createAndAddResource(type, health, distance) {
     army.push(createResource(type, health, distance));
 }
 
-function checkResourcesReadyToMove(distance) {
+function isResourcesReadyToMove(distance) {
     army.every(function (res) {
         return res.distance >= distance;
     })
 }
 
-function checkResourcesReadyToFight(danger) {
+function isResourcesReadyToFight(danger) {
     army.every(function (res) {
         return res.health > danger;
     })
 }
 
-function resourceRestore(index, characteristic) {
+function restoreResource(index, characteristic) {
     var resource = army[index];
     if (!resource) return;
     resource[characteristic] = resource["max" + characteristic];
 }
 
+function attackResource(index, points) {
+    var resource = army[index];
+    if (!resource) return;
+    resource.health -= points;
+}
+
 function restoreResources(index, characteristic) {
     var characteristic = characteristic ? characteristic : "health";
-    index ? resourceRestore(index, characteristic) :
+    index ? restoreResource(index, characteristic) :
         army.forEach(function (res, resourceIndex) {
-            resourceRestore(resourceIndex, characteristic);
+            restoreResource(resourceIndex, characteristic);
         })
 }
 
@@ -62,11 +67,57 @@ function cloneResource(element) {
     return newResource;
 }
 
+function toPixel(value) {
+    return value + "px";
+}
+
+function bar(elementClass, value, resourceWidth, barHeight) {
+    var barDiv = document.createElement("div"),
+        barInnerDiv = document.createElement("div"),
+        barInfo = document.createElement("div"),
+        valueLeft = resourceWidth * (value / 100),
+        percent = valueLeft / resourceWidth * 100;
+
+    barDiv.className = elementClass;
+    barDiv.style.width = resourceWidth;
+
+    barInfo.innerHTML = percent + "%";
+    barInfo.className = "bar-info";
+
+    barInnerDiv.style.width = toPixel(valueLeft);
+    barInnerDiv.style.height = toPixel(barHeight);
+    barInnerDiv.className = "bar-color";
+
+    
+    barDiv.appendChild(barInnerDiv);
+    barDiv.appendChild(barInfo);
+
+    return barDiv;
+}
+
 function render(element) {
+    var resourceWidth = 100,
+        resourceHeight = 100,
+        barHeight = 20;
+
     element.innerHTML = "";
-    army.forEach(function () {
-        var resDiv = document.createElement("div");
+    army.forEach(function (resource) {
+        var resDiv = document.createElement("div"),
+            healthBar = bar("health", resource.health / resource.maxHealth * 100, resourceWidth, barHeight),
+            staminaBar = bar("stamina", resource.distance / resource.maxDistance * 100, resourceWidth, barHeight),
+            infoDiv = document.createElement("div");
+
         resDiv.classList = ["resource"];
+        resDiv.style.width = toPixel(resourceWidth);
+        resDiv.style.height = toPixel(resourceHeight);
+
+
+        infoDiv.className = ["resource-name"];
+        infoDiv.innerHTML = resource.type;
+
+        resDiv.appendChild(healthBar);
+        resDiv.appendChild(staminaBar);
+        resDiv.appendChild(infoDiv);
         element.appendChild(resDiv);
     });
 }
@@ -83,4 +134,10 @@ var newForce = [
 
 combineResources(newForce);
 
+
 render(document.getElementById("wrapper"));
+
+setTimeout(function () {
+    attackResource(0, 50);
+    render(document.getElementById("wrapper"));
+}, 1000);
