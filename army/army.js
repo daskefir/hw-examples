b6f033780a546d47039433adddd83a0ece64f772
Denis <fiwhy@mail.ru>
2017-12-21 14:06:47 +0200

    Added damage to clone method

--- army/army.js
+++ army/army.js
@@ -13,15 +13,15 @@ Resource.prototype.isReadyToFight = function (damage) {
     return damage ? (this.health - damage) > 0 : this.health;
 }
 
-Resource.prototype.restore = function() {
+Resource.prototype.restore = function () {
     this.health = this.maxHealth;
     this.distance = this.maxDistance;
 }
 
-Resource.prototype.clone = function() {
-    return new Resource(this.type, this.maxHealth, this.maxDistance);
+Resource.prototype.clone = function () {
+    return new Resource(this.type, this.damage, this.maxHealth, this.maxDistance);
 }
 
-Resource.prototype.attackedBy = function(target) {
+Resource.prototype.attackedBy = function (target) {
     this.health -= target.damage;
 }
